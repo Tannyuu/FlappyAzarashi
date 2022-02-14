2bd61b67f4d9f29705e621ee0aec521777e93016
hogehoge <hogehoge@gmail.com>
2022-02-14 13:58:55 +0900

    p349まで作成

---
+++ Assets/Scripts/ScrollObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollObject : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float startPosition;
+    public float endPosition;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+
+        if(transform.position.x <= endPosition)
+        {
+            ScrollEnd();
+        }
+    }
+
+    void ScrollEnd()
+    {
+        /*
+        Debug.Log(transform.position.x);
+        transform.position = new Vector3(8.0f, 0, 0); //隙間ができる
+        */
+
+        
+        float diff = transform.position.x - endPosition;
+        Vector3 restartPosition = transform.position;
+        restartPosition.x = startPosition + diff;
+        transform.position = restartPosition;
+        
+
+        SendMessage("OnScrollEnd", SendMessageOptions.DontRequireReceiver);//引数受け取れない スペルミスを教えてくれない アタッチされてるオブジェクトだけ呼び出せる .gameObject → .GetComponentのほうが自由に呼び出せる
+    }
+}
